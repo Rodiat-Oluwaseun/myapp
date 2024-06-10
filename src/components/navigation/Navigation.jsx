53bf095f2df29258cd85c1e0b5f4aae691e93cec
rodiat-oluwaseun <yemirolayemi@gmail.com>
2024-06-10 01:40:31 +0100

    Navigation bar completed and header started.

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -33,36 +33,36 @@ const Navigation = () => {
         <div className="navbar-links-container">
           <Menu />
         </div>
-        <div className="navbar-sign">
-          <p>Sign in</p>
-          <button type="button"> Sign up</button>
-        </div>
-        <div className="navbar-menu">
-          {toggleMenu ? (
-            <RiCloseLine
-              color="#fff"
-              size={27}
-              onClick={() => setToggleMenu(false)}
-            />
-          ) : (
-            <RiMenu3Line
-              color="#fff"
-              size={27}
-              onClick={() => setToggleMenu(true)}
-            />
-          )}
-          {toggleMenu && (
-            <div className="navbar-menu-container scale-up-center">
-              <div className="navbar-menu-container-links">
-                <Menu />
-                <div className="navbar-menu-container-links-sign">
-                  <p>Sign in</p>
-                  <button type="button"> Sign up</button>
-                </div>
+      </div>
+      <div className="navbar-sign">
+        <p>Sign in</p>
+        <button type="button"> Sign up</button>
+      </div>
+      <div className="navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="navbar-menu-container scale-up-center">
+            <div className="navbar-menu-container-links">
+              <Menu />
+              <div className="navbar-menu-container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import AI from "../../assets/ai.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header-section-padding" id="home">
+      <div className="header-content">
+        <h1 className="gradient-text">
+          Let's Build Someting amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance Indulgence unpleasing Not
+          thought all exercise blessing indulgence way everything joy alteration
+          boisterous the attachement .Party we years to order allowed asked of.
+        </p>
+        <div className="header-content-input">
+          <input type="email" placeholder="your email address" />
+          <button type="button">Get started</button>
+        </div>
+        <div className="header-content-people">
+          <img src={people} alt="people" />
+          <p>1,600 people required access visit in the last 24 hoours</p>
+        </div>
+
+        <div className="header-image">
+          <img src={AI} alt="ai" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
